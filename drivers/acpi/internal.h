9753c7617ac2f23559af9a9573b543a90afdeb04
Hans de Goede <hdegoede@redhat.com>
2022-12-08 14:23:34 +0000

    BACKPORT: ACPI: EC: Fix EC address space handler unregistration
    [ Upstream commit a5072078dbfaa9d70130805766dfa34bbb7bf2a7 ]
    
    When an ECDT table is present the EC address space handler gets registered
    on the root node. So to unregister it properly the unregister call also
    must be done on the root node.
    
    Store the ACPI handle used for the acpi_install_address_space_handler()
    call and use te same handle for the acpi_remove_address_space_handler()
    call.
    
    Reported-by: Rafael J. Wysocki <rafael@kernel.org>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Stable-dep-of: f6484cadbcaf ("ACPI: EC: clean up handlers on probe failure in acpi_ec_setup()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/acpi/internal.h
+++ drivers/acpi/internal.h
@@ -169,7 +169,9 @@ static inline void acpi_early_processor_osc(void) {}
    -------------------------------------------------------------------------- */
 struct acpi_ec {
 	acpi_handle handle;
-	u32 gpe;
+	acpi_handle address_space_handler_holder;
+	int gpe;
+	int irq;
 	unsigned long command_addr;
 	unsigned long data_addr;
 	bool global_lock;
