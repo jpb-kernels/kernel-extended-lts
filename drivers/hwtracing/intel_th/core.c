00af0c965cfc3cbd4ffbcc4314dac23758f80384
Johan Hovold <johan@kernel.org>
2025-12-08 16:35:23 +0100

    BACKPORT: intel_th: fix device leak on output open()
    commit 95fc36a234da24bbc5f476f8104a5a15f99ed3e3 upstream.
    
    Make sure to drop the reference taken when looking up the th device
    during output device open() on errors and on close().
    
    Note that a recent commit fixed the leak in a couple of open() error
    paths but not all of them, and the reference is still leaking on
    successful open().
    
    Fixes: 39f4034693b7 ("intel_th: Add driver infrastructure for Intel(R) Trace Hub devices")
    Fixes: 6d5925b667e4 ("intel_th: Fix error handling in intel_th_output_open")
    Cc: stable@vger.kernel.org	# 4.4: 6d5925b667e4
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Ma Ke <make24@iscas.ac.cn>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Link: https://patch.msgid.link/20251208153524.68637-2-johan@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hwtracing/intel_th/core.c
+++ drivers/hwtracing/intel_th/core.c
@@ -810,8 +810,13 @@ static int intel_th_output_open(struct inode *inode, struct file *file)
 	int err;
 
 	dev = bus_find_device_by_devt(&intel_th_bus, inode->i_rdev);
-	if (!dev || !dev->driver)
+	if (!dev)
 		return -ENODEV;
+
+	if (!dev->driver) {
+		err = -ENODEV;
+		goto out_put_device;
+	}
 
 	thdrv = to_intel_th_driver(dev->driver);
 	fops = fops_get(thdrv->fops);
@@ -828,10 +833,25 @@ static int intel_th_output_open(struct inode *inode, struct file *file)
 	}
 
 	return 0;
+
+out_put_device:
+	put_device(dev);
+
+	return err;
+}
+
+static int intel_th_output_release(struct inode *inode, struct file *file)
+{
+	struct intel_th_device *thdev = file->private_data;
+
+	put_device(&thdev->dev);
+
+	return 0;
 }
 
 static const struct file_operations intel_th_output_fops = {
 	.open	= intel_th_output_open,
+	.release = intel_th_output_release,
 	.llseek	= noop_llseek,
 };
 
