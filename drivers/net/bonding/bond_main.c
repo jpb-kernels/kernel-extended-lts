732ca422871f967933f411cc012eff1dbea6192d
Eric Dumazet <edumazet@google.com>
2026-01-20 16:17:44 +0000

    BACKPORT: bonding: provide a net pointer to __skb_flow_dissect()
    commit 5f9b329096596b7e53e07d041d7fca4cbe1be752 upstream.
    
    After 3cbf4ffba5ee ("net: plumb network namespace into __skb_flow_dissect")
    we have to provide a net pointer to __skb_flow_dissect(),
    either via skb->dev, skb->sk, or a user provided pointer.
    
    In the following case, syzbot was able to cook a bare skb.
    
    WARNING: net/core/flow_dissector.c:1131 at __skb_flow_dissect+0xb57/0x68b0 net/core/flow_dissector.c:1131, CPU#1: syz.2.1418/11053
    Call Trace:
     <TASK>
      bond_flow_dissect drivers/net/bonding/bond_main.c:4093 [inline]
      __bond_xmit_hash+0x2d7/0xba0 drivers/net/bonding/bond_main.c:4157
      bond_xmit_hash_xdp drivers/net/bonding/bond_main.c:4208 [inline]
      bond_xdp_xmit_3ad_xor_slave_get drivers/net/bonding/bond_main.c:5139 [inline]
      bond_xdp_get_xmit_slave+0x1fd/0x710 drivers/net/bonding/bond_main.c:5515
      xdp_master_redirect+0x13f/0x2c0 net/core/filter.c:4388
      bpf_prog_run_xdp include/net/xdp.h:700 [inline]
      bpf_test_run+0x6b2/0x7d0 net/bpf/test_run.c:421
      bpf_prog_test_run_xdp+0x795/0x10e0 net/bpf/test_run.c:1390
      bpf_prog_test_run+0x2c7/0x340 kernel/bpf/syscall.c:4703
      __sys_bpf+0x562/0x860 kernel/bpf/syscall.c:6182
      __do_sys_bpf kernel/bpf/syscall.c:6274 [inline]
      __se_sys_bpf kernel/bpf/syscall.c:6272 [inline]
      __x64_sys_bpf+0x7c/0x90 kernel/bpf/syscall.c:6272
      do_syscall_x64 arch/x86/entry/syscall_64.c:63 [inline]
      do_syscall_64+0xec/0xf80 arch/x86/entry/syscall_64.c:94
    
    Fixes: 58deb77cc52d ("bonding: balance ICMP echoes in layer3+4 mode")
    Reported-by: syzbot+c46409299c70a221415e@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/netdev/696faa23.050a0220.4cb9c.001f.GAE@google.com/T/#u
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Matteo Croce <mcroce@redhat.com>
    Acked-by: Stanislav Fomichev <sdf@fomichev.me>
    Link: https://patch.msgid.link/20260120161744.1893263-1-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -3340,8 +3340,55 @@ static bool bond_flow_dissect(struct bonding *bond, struct sk_buff *skb,
 	} else {
 		return false;
 	}
-	if (bond->params.xmit_policy == BOND_XMIT_POLICY_LAYER34 && proto >= 0)
-		fk->ports.ports = skb_flow_get_ports(skb, noff, proto);
+
+	if (l34 && *proto >= 0)
+		fk->ports.ports = skb_flow_get_ports(skb, *noff, *proto);
+
+	return true;
+}
+
+/* Extract the appropriate headers based on bond's xmit policy */
+static bool bond_flow_dissect(struct bonding *bond, struct sk_buff *skb,
+			      struct flow_keys *fk)
+{
+	bool l34 = bond->params.xmit_policy == BOND_XMIT_POLICY_LAYER34;
+	int noff, proto = -1;
+
+	if (bond->params.xmit_policy > BOND_XMIT_POLICY_LAYER23) {
+		memset(fk, 0, sizeof(*fk));
+		return __skb_flow_dissect(dev_net(bond->dev), skb,
+					  &flow_keys_bonding, fk, NULL, 0, 0,
+					  0, 0);
+	}
+
+	fk->ports.ports = 0;
+	memset(&fk->icmp, 0, sizeof(fk->icmp));
+	noff = skb_network_offset(skb);
+	if (!bond_flow_ip(skb, fk, &noff, &proto, l34))
+		return false;
+
+	/* ICMP error packets contains at least 8 bytes of the header
+	 * of the packet which generated the error. Use this information
+	 * to correlate ICMP error packets within the same flow which
+	 * generated the error.
+	 */
+	if (proto == IPPROTO_ICMP || proto == IPPROTO_ICMPV6) {
+		skb_flow_get_icmp_tci(skb, &fk->icmp, skb->data,
+				      skb_transport_offset(skb),
+				      skb_headlen(skb));
+		if (proto == IPPROTO_ICMP) {
+			if (!icmp_is_err(fk->icmp.type))
+				return true;
+
+			noff += sizeof(struct icmphdr);
+		} else if (proto == IPPROTO_ICMPV6) {
+			if (!icmpv6_is_err(fk->icmp.type))
+				return true;
+
+			noff += sizeof(struct icmp6hdr);
+		}
+		return bond_flow_ip(skb, fk, &noff, &proto, l34);
+	}
 
 	return true;
 }
