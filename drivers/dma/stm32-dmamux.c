8a869662c30b4b299c4026cd36d47a27969adc87
Johan Hovold <johan@kernel.org>
2026-01-21 07:40:36 -0500

    dmaengine: stm32: dmamux: fix OF node leak on route allocation failure
    [ Upstream commit b1b590a590af13ded598e70f0b72bc1e515787a1 ]
    
    Make sure to drop the reference taken to the DMA master OF node also on
    late route allocation failures.
    
    Fixes: df7e762db5f6 ("dmaengine: Add STM32 DMAMUX driver")
    Cc: stable@vger.kernel.org      # 4.15
    Cc: Pierre-Yves MORDRET <pierre-yves.mordret@foss.st.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Reviewed-by: Amelie Delaunay <amelie.delaunay@foss.st.com>
    Link: https://patch.msgid.link/20251117161258.10679-12-johan@kernel.org
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/dma/stm32-dmamux.c
+++ drivers/dma/stm32-dmamux.c
@@ -138,7 +138,7 @@ static void *stm32_dmamux_route_allocate(struct of_phandle_args *dma_spec,
 	ret = pm_runtime_get_sync(&pdev->dev);
 	if (ret < 0) {
 		spin_unlock_irqrestore(&dmamux->lock, flags);
-		goto error;
+		goto err_put_dma_spec_np;
 	}
 	spin_unlock_irqrestore(&dmamux->lock, flags);
 
@@ -158,6 +158,8 @@ static void *stm32_dmamux_route_allocate(struct of_phandle_args *dma_spec,
 
 	return mux;
 
+err_put_dma_spec_np:
+	of_node_put(dma_spec->np);
 error:
 	clear_bit(mux->chan_id, dmamux->dma_inuse);
 
