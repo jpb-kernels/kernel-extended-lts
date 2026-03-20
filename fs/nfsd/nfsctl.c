f6e559ead16ade7f9d1c4ebb9d6fd8c59ee8f19d
Tom Rix <trix@redhat.com>
2026-03-20 11:39:38 +0000

    BACKPORT: nfsd: define exports_proc_ops with CONFIG_PROC_FS
    [ Upstream commit 340086da9a87820b40601141a0e9e87c954ac006 ]
    
    gcc with W=1 and ! CONFIG_PROC_FS
    fs/nfsd/nfsctl.c:161:30: error: ‘exports_proc_ops’
      defined but not used [-Werror=unused-const-variable=]
      161 | static const struct proc_ops exports_proc_ops = {
          |                              ^~~~~~~~~~~~~~~~
    
    The only use of exports_proc_ops is when CONFIG_PROC_FS
    is defined, so its definition should be likewise conditional.
    
    Signed-off-by: Tom Rix <trix@redhat.com>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Stable-dep-of: e7fcf179b82d ("NFSD: Hold net reference for the lifetime of /proc/fs/nfs/exports fd")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -152,18 +152,6 @@ static int exports_net_open(struct net *net, struct file *file)
 	return 0;
 }
 
-static int exports_proc_open(struct inode *inode, struct file *file)
-{
-	return exports_net_open(current->nsproxy->net_ns, file);
-}
-
-static const struct file_operations exports_proc_operations = {
-	.open		= exports_proc_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= seq_release,
-};
-
 static int exports_nfsd_open(struct inode *inode, struct file *file)
 {
 	return exports_net_open(inode->i_sb->s_fs_info, file);
@@ -394,12 +382,12 @@ static ssize_t write_filehandle(struct file *file, char *buf, size_t size)
 	auth_domain_put(dom);
 	if (len)
 		return len;
-	
+
 	mesg = buf;
 	len = SIMPLE_TRANSACTION_LIMIT;
 	qword_addhex(&mesg, &len, (char*)&fh.fh_base, fh.fh_size);
 	mesg[-1] = '\n';
-	return mesg - buf;	
+	return mesg - buf;
 }
 
 /**
@@ -1432,6 +1420,19 @@ static struct file_system_type nfsd_fs_type = {
 MODULE_ALIAS_FS("nfsd");
 
 #ifdef CONFIG_PROC_FS
+
+static int exports_proc_open(struct inode *inode, struct file *file)
+{
+	return exports_net_open(current->nsproxy->net_ns, file);
+}
+
+static const struct proc_ops exports_proc_ops = {
+	.proc_open	= exports_proc_open,
+	.proc_read	= seq_read,
+	.proc_lseek	= seq_lseek,
+	.proc_release	= seq_release,
+};
+
 static int create_proc_exports_entry(void)
 {
 	struct proc_dir_entry *entry;
