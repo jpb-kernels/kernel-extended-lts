a163efc3ae313ac08481d85f69f07b4977638415
Mingzhe Zou <mingzhe.zou@easystack.cn>
2026-04-03 12:21:35 +0800

    bcache: fix uninitialized closure object
    commit 20a8e451ec1c7e99060b1bbaaad03ce88c39ddb8 upstream.
    
    In the previous patch ("bcache: fix cached_dev.sb_bio use-after-free and
    crash"), we adopted a simple modification suggestion from AI to fix the
    use-after-free.
    
    But in actual testing, we found an extreme case where the device is
    stopped before calling bch_write_bdev_super().
    
    At this point, struct closure sb_write has not been initialized yet.
    For this patch, we ensure that sb_bio has been completed via
    sb_write_mutex.
    
    Signed-off-by: Mingzhe Zou <mingzhe.zou@easystack.cn>
    Signed-off-by: Coly Li <colyli@fnnas.com>
    Link: https://patch.msgid.link/20260403042135.2221247-1-colyli@fnnas.com
    Fixes: fec114a98b87 ("bcache: fix cached_dev.sb_bio use-after-free and crash")
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -1291,7 +1291,8 @@ static void cached_dev_free(struct closure *cl)
 	 * The sb_bio is embedded in struct cached_dev, so we must
 	 * ensure no I/O is in progress.
 	 */
-	closure_sync(&dc->sb_write);
+	down(&dc->sb_write_mutex);
+	up(&dc->sb_write_mutex);
 
 	if (dc->sb_bio.bi_inline_vecs[0].bv_page)
 		put_page(bio_first_page_all(&dc->sb_bio));
