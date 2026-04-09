88bb5de6b41d2c8cb9e1d17cc9ca581df6b4892b
jpb <jpb44173@gmail.com>
2026-04-09 14:13:23 -0700

    Revert "BACKPORT: bpf: Reject narrower access to pointer ctx fields"
    This reverts commit 0be6db72ad96da2a5318a955102805ab0e798ed6.

--- net/core/filter.c
+++ net/core/filter.c
@@ -6625,7 +6625,7 @@ static bool bpf_skb_is_valid_access(int off, int size, enum bpf_access_type type
 		if (size != sizeof(__u64))
 			return false;
 		break;
-	case bpf_ctx_range_ptr(struct __sk_buff, sk):
+	case offsetof(struct __sk_buff, sk):
 		if (type == BPF_WRITE || size != sizeof(__u64))
 			return false;
 		info->reg_type = PTR_TO_SOCK_COMMON_OR_NULL;
@@ -7132,7 +7132,7 @@ static bool sock_addr_is_valid_access(int off, int size,
 		if (size != size_default)
 			return false;
 		break;
-	case bpf_ctx_range_ptr(struct bpf_sock_addr, sk):
+	case offsetof(struct bpf_sock_addr, sk):
 		if (type != BPF_READ)
 			return false;
 		if (size != sizeof(__u64))
@@ -7186,25 +7186,11 @@ static bool sock_ops_is_valid_access(int off, int size,
 			if (size != sizeof(__u64))
 				return false;
 			break;
-		case bpf_ctx_range_ptr(struct bpf_sock_ops, sk):
+		case offsetof(struct bpf_sock_ops, sk):
 			if (size != sizeof(__u64))
 				return false;
 			info->reg_type = PTR_TO_SOCKET_OR_NULL;
 			break;
-		case bpf_ctx_range_ptr(struct bpf_sock_ops, skb_data):
-			if (size != sizeof(__u64))
-				return false;
-			info->reg_type = PTR_TO_PACKET;
-			break;
-		case bpf_ctx_range_ptr(struct bpf_sock_ops, skb_data_end):
-			if (size != sizeof(__u64))
-				return false;
-			info->reg_type = PTR_TO_PACKET_END;
-			break;
-		case offsetof(struct bpf_sock_ops, skb_tcp_flags):
-			bpf_ctx_record_field_size(info, size_default);
-			return bpf_ctx_narrow_access_ok(off, size,
-							size_default);
 		default:
 			if (size != size_default)
 				return false;
@@ -7270,20 +7256,15 @@ static bool sk_msg_is_valid_access(int off, int size,
 		return false;
 
 	switch (off) {
-	case bpf_ctx_range_ptr(struct sk_msg_md, data):
+	case offsetof(struct sk_msg_md, data):
 		info->reg_type = PTR_TO_PACKET;
 		if (size != sizeof(__u64))
 			return false;
 		break;
-	case bpf_ctx_range_ptr(struct sk_msg_md, data_end):
+	case offsetof(struct sk_msg_md, data_end):
 		info->reg_type = PTR_TO_PACKET_END;
 		if (size != sizeof(__u64))
 			return false;
-		break;
-	case bpf_ctx_range_ptr(struct sk_msg_md, sk):
-		if (size != sizeof(__u64))
-			return false;
-		info->reg_type = PTR_TO_SOCKET;
 		break;
 	case bpf_ctx_range(struct sk_msg_md, family):
 	case bpf_ctx_range(struct sk_msg_md, remote_ip4):
@@ -9138,188 +9119,4 @@ const struct bpf_verifier_ops sk_reuseport_verifier_ops = {
 
 const struct bpf_prog_ops sk_reuseport_prog_ops = {
 };
-
-DEFINE_STATIC_KEY_FALSE(bpf_sk_lookup_enabled);
-EXPORT_SYMBOL(bpf_sk_lookup_enabled);
-
-BPF_CALL_3(bpf_sk_lookup_assign, struct bpf_sk_lookup_kern *, ctx,
-	   struct sock *, sk, u64, flags)
-{
-	if (unlikely(flags & ~(BPF_SK_LOOKUP_F_REPLACE |
-			       BPF_SK_LOOKUP_F_NO_REUSEPORT)))
-		return -EINVAL;
-	if (unlikely(sk && sk_is_refcounted(sk)))
-		return -ESOCKTNOSUPPORT; /* reject non-RCU freed sockets */
-	if (unlikely(sk && sk->sk_state == TCP_ESTABLISHED))
-		return -ESOCKTNOSUPPORT; /* reject connected sockets */
-
-	/* Check if socket is suitable for packet L3/L4 protocol */
-	if (sk && sk->sk_protocol != ctx->protocol)
-		return -EPROTOTYPE;
-	if (sk && sk->sk_family != ctx->family &&
-	    (sk->sk_family == AF_INET || ipv6_only_sock(sk)))
-		return -EAFNOSUPPORT;
-
-	if (ctx->selected_sk && !(flags & BPF_SK_LOOKUP_F_REPLACE))
-		return -EEXIST;
-
-	/* Select socket as lookup result */
-	ctx->selected_sk = sk;
-	ctx->no_reuseport = flags & BPF_SK_LOOKUP_F_NO_REUSEPORT;
-	return 0;
-}
-
-static const struct bpf_func_proto bpf_sk_lookup_assign_proto = {
-	.func		= bpf_sk_lookup_assign,
-	.gpl_only	= false,
-	.ret_type	= RET_INTEGER,
-	.arg1_type	= ARG_PTR_TO_CTX,
-	.arg2_type	= ARG_PTR_TO_SOCKET_OR_NULL,
-	.arg3_type	= ARG_ANYTHING,
-};
-
-static const struct bpf_func_proto *
-sk_lookup_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
-{
-	switch (func_id) {
-	case BPF_FUNC_perf_event_output:
-		return &bpf_event_output_data_proto;
-	case BPF_FUNC_sk_assign:
-		return &bpf_sk_lookup_assign_proto;
-	case BPF_FUNC_sk_release:
-		return &bpf_sk_release_proto;
-	default:
-		return bpf_sk_base_func_proto(func_id);
-	}
-}
-
-static bool sk_lookup_is_valid_access(int off, int size,
-				      enum bpf_access_type type,
-				      const struct bpf_prog *prog,
-				      struct bpf_insn_access_aux *info)
-{
-	if (off < 0 || off >= sizeof(struct bpf_sk_lookup))
-		return false;
-	if (off % size != 0)
-		return false;
-	if (type != BPF_READ)
-		return false;
-
-	switch (off) {
-	case bpf_ctx_range_ptr(struct bpf_sk_lookup, sk):
-		info->reg_type = PTR_TO_SOCKET_OR_NULL;
-		return size == sizeof(__u64);
-
-	case bpf_ctx_range(struct bpf_sk_lookup, family):
-	case bpf_ctx_range(struct bpf_sk_lookup, protocol):
-	case bpf_ctx_range(struct bpf_sk_lookup, remote_ip4):
-	case bpf_ctx_range(struct bpf_sk_lookup, local_ip4):
-	case bpf_ctx_range_till(struct bpf_sk_lookup, remote_ip6[0], remote_ip6[3]):
-	case bpf_ctx_range_till(struct bpf_sk_lookup, local_ip6[0], local_ip6[3]):
-	case bpf_ctx_range(struct bpf_sk_lookup, remote_port):
-	case bpf_ctx_range(struct bpf_sk_lookup, local_port):
-		bpf_ctx_record_field_size(info, sizeof(__u32));
-		return bpf_ctx_narrow_access_ok(off, size, sizeof(__u32));
-
-	default:
-		return false;
-	}
-}
-
-static u32 sk_lookup_convert_ctx_access(enum bpf_access_type type,
-					const struct bpf_insn *si,
-					struct bpf_insn *insn_buf,
-					struct bpf_prog *prog,
-					u32 *target_size)
-{
-	struct bpf_insn *insn = insn_buf;
-
-	switch (si->off) {
-	case offsetof(struct bpf_sk_lookup, sk):
-		*insn++ = BPF_LDX_MEM(BPF_SIZEOF(void *), si->dst_reg, si->src_reg,
-				      offsetof(struct bpf_sk_lookup_kern, selected_sk));
-		break;
-
-	case offsetof(struct bpf_sk_lookup, family):
-		*insn++ = BPF_LDX_MEM(BPF_H, si->dst_reg, si->src_reg,
-				      bpf_target_off(struct bpf_sk_lookup_kern,
-						     family, 2, target_size));
-		break;
-
-	case offsetof(struct bpf_sk_lookup, protocol):
-		*insn++ = BPF_LDX_MEM(BPF_H, si->dst_reg, si->src_reg,
-				      bpf_target_off(struct bpf_sk_lookup_kern,
-						     protocol, 2, target_size));
-		break;
-
-	case offsetof(struct bpf_sk_lookup, remote_ip4):
-		*insn++ = BPF_LDX_MEM(BPF_W, si->dst_reg, si->src_reg,
-				      bpf_target_off(struct bpf_sk_lookup_kern,
-						     v4.saddr, 4, target_size));
-		break;
-
-	case offsetof(struct bpf_sk_lookup, local_ip4):
-		*insn++ = BPF_LDX_MEM(BPF_W, si->dst_reg, si->src_reg,
-				      bpf_target_off(struct bpf_sk_lookup_kern,
-						     v4.daddr, 4, target_size));
-		break;
-
-	case bpf_ctx_range_till(struct bpf_sk_lookup,
-				remote_ip6[0], remote_ip6[3]): {
-#if IS_ENABLED(CONFIG_IPV6)
-		int off = si->off;
-
-		off -= offsetof(struct bpf_sk_lookup, remote_ip6[0]);
-		off += bpf_target_off(struct in6_addr, s6_addr32[0], 4, target_size);
-		*insn++ = BPF_LDX_MEM(BPF_SIZEOF(void *), si->dst_reg, si->src_reg,
-				      offsetof(struct bpf_sk_lookup_kern, v6.saddr));
-		*insn++ = BPF_JMP_IMM(BPF_JEQ, si->dst_reg, 0, 1);
-		*insn++ = BPF_LDX_MEM(BPF_W, si->dst_reg, si->dst_reg, off);
-#else
-		*insn++ = BPF_MOV32_IMM(si->dst_reg, 0);
-#endif
-		break;
-	}
-	case bpf_ctx_range_till(struct bpf_sk_lookup,
-				local_ip6[0], local_ip6[3]): {
-#if IS_ENABLED(CONFIG_IPV6)
-		int off = si->off;
-
-		off -= offsetof(struct bpf_sk_lookup, local_ip6[0]);
-		off += bpf_target_off(struct in6_addr, s6_addr32[0], 4, target_size);
-		*insn++ = BPF_LDX_MEM(BPF_SIZEOF(void *), si->dst_reg, si->src_reg,
-				      offsetof(struct bpf_sk_lookup_kern, v6.daddr));
-		*insn++ = BPF_JMP_IMM(BPF_JEQ, si->dst_reg, 0, 1);
-		*insn++ = BPF_LDX_MEM(BPF_W, si->dst_reg, si->dst_reg, off);
-#else
-		*insn++ = BPF_MOV32_IMM(si->dst_reg, 0);
-#endif
-		break;
-	}
-	case offsetof(struct bpf_sk_lookup, remote_port):
-		*insn++ = BPF_LDX_MEM(BPF_H, si->dst_reg, si->src_reg,
-				      bpf_target_off(struct bpf_sk_lookup_kern,
-						     sport, 2, target_size));
-		break;
-
-	case offsetof(struct bpf_sk_lookup, local_port):
-		*insn++ = BPF_LDX_MEM(BPF_H, si->dst_reg, si->src_reg,
-				      bpf_target_off(struct bpf_sk_lookup_kern,
-						     dport, 2, target_size));
-		break;
-	}
-
-	return insn - insn_buf;
-}
-
-const struct bpf_prog_ops sk_lookup_prog_ops = {
-	.test_run = bpf_prog_test_run_sk_lookup,
-};
-
-const struct bpf_verifier_ops sk_lookup_verifier_ops = {
-	.get_func_proto		= sk_lookup_func_proto,
-	.is_valid_access	= sk_lookup_is_valid_access,
-	.convert_ctx_access	= sk_lookup_convert_ctx_access,
-};
-
 #endif /* CONFIG_INET */
