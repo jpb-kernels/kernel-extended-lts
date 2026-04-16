940a2a055c03c2b6cb087062e4d1581e9619c144
Eric Dumazet <edumazet@google.com>
2026-04-16 21:03:18 +0100

    BACKPORT: tcp: annotate data-races around (tp->write_seq - tp->snd_nxt)
    [ Upstream commit 3a63b3d160560ef51e43fb4c880a5cde8078053c ]
    
    tcp_get_timestamping_opt_stats() intentionally runs lockless, we must
    add READ_ONCE() annotations to keep KCSAN happy.
    
    WRITE_ONCE() annotations are already present.
    
    Fixes: e08ab0b377a1 ("tcp: add bytes not sent to SCM_TIMESTAMPING_OPT_STATS")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Link: https://patch.msgid.link/20260416200319.3608680-14-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3454,6 +3454,12 @@ struct sk_buff *tcp_get_timestamping_opt_stats(const struct sock *sk)
 	nla_put_u32(stats, TCP_NLA_DSACK_DUPS, tp->dsack_dups);
 	nla_put_u32(stats, TCP_NLA_REORD_SEEN, tp->reord_seen);
 	nla_put_u32(stats, TCP_NLA_SRTT, tp->srtt_us >> 3);
+	nla_put_u16(stats, TCP_NLA_TIMEOUT_REHASH, tp->timeout_rehash);
+	nla_put_u32(stats, TCP_NLA_BYTES_NOTSENT,
+		    max_t(int, 0,
+			  READ_ONCE(tp->write_seq) - READ_ONCE(tp->snd_nxt)));
+	nla_put_u64_64bit(stats, TCP_NLA_EDT, orig_skb->skb_mstamp_ns,
+			  TCP_NLA_PAD);
 
 	return stats;
 }
