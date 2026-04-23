3325291ac267b09c5a9c213167142d0212d3874e
Sasha Levin <sashal@kernel.org>
2026-04-23 09:50:40 -0400

    Revert "scsi: ufs: core: Improve SCSI abort handling"
    This reverts commit 133811fbc1cc171477281c829eb5fd567f013ba7.
    
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -6238,7 +6238,6 @@ cleanup:
 	spin_unlock_irqrestore(host->host_lock, flags);
 
 out:
-		lrbp->cmd = NULL;
 		err = SUCCESS;
 	} else {
 		dev_err(hba->dev, "%s: failed with err %d\n", __func__, err);
