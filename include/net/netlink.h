5b7c4f9cc9bf50e1cd15a6c9ed9a9bdc6352459a
jpb <jpb44173@gmail.com>
2026-04-26 08:46:50 +0100

    Revert "BACKPORT: netlink: introduce NLA_POLICY_MAX_BE"
    This reverts commit 122fd169

--- include/net/netlink.h
+++ include/net/netlink.h
@@ -293,10 +293,13 @@ struct nla_policy {
 	u8		validation_type;
 	u16		len;
 	union {
-		/**
-		 * @strict_start_type: first attribute to validate strictly
-		 *
-		 * This entry is special, and used for the attribute at index 0
+		const void *validation_data;
+		struct {
+			s16 min, max;
+		};
+		int (*validate)(const struct nlattr *attr,
+				struct netlink_ext_ack *extack);
+		/* This entry is special, and used for the attribute at index 0
 		 * only, and specifies special data about the policy, namely it
 		 * specifies the "boundary type" where strict length validation
 		 * starts for any attribute types >= this value, also, strict
@@ -314,20 +317,6 @@ struct nla_policy {
 		 * was added to enforce strict validation from thereon.
 		 */
 		u16 strict_start_type;
-
-		/* private: use NLA_POLICY_*() to set */
-		const u32 bitfield32_valid;
-		const u32 mask;
-		const char *reject_message;
-		const struct nla_policy *nested_policy;
-		struct netlink_range_validation *range;
-		struct netlink_range_validation_signed *range_signed;
-		struct {
-			s16 min, max;
-			u8 network_byte_order:1;
-		};
-		int (*validate)(const struct nlattr *attr,
-				struct netlink_ext_ack *extack);
 	};
 };
 
@@ -377,14 +366,6 @@ struct nla_policy {
 	.type = NLA_ENSURE_INT_TYPE(tp),		\
 	.validation_type = NLA_VALIDATE_MAX,		\
 	.max = _max,					\
-	.network_byte_order = 0,			\
-}
-
-#define NLA_POLICY_MAX_BE(tp, _max) {			\
-	.type = NLA_ENSURE_UINT_TYPE(tp),		\
-	.validation_type = NLA_VALIDATE_MAX,		\
-	.max = _max,					\
-	.network_byte_order = 1,			\
 }
 
 #define NLA_POLICY_VALIDATE_FN(tp, fn, ...) {		\
