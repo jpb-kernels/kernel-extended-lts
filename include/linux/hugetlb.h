723d2cc20c78097675eb419d2a27724adac5c090
jpb <jpb44173@gmail.com>
2026-04-26 08:23:37 +0100

    Revert "mm/hugetlb: fix hugetlb_pmd_shared()"
    This reverts commit 14bee1e5b4ee8e08102c495f0e343d443bb27430.

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -751,7 +751,7 @@ static inline spinlock_t *huge_pte_lock(struct hstate *h,
 #ifdef CONFIG_ARCH_WANT_HUGE_PMD_SHARE
 static inline bool hugetlb_pmd_shared(pte_t *pte)
 {
-	return atomic_read(&virt_to_page(pte)->pt_share_count);
+	return page_count(virt_to_page(pte)) > 1;
 }
 #else
 static inline bool hugetlb_pmd_shared(pte_t *pte)
